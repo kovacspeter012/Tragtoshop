97a17ef4780feabc639a21067b78ce7cd34be71e
BuildTools <unconfigured@null.spigotmc.org>
2022-03-31 17:15:30 +0200

    sdvsv

--- tragta.js
+++ tragta.js
@@ -1,4 +1,30 @@
 function showImg(parameter) {
     document.querySelector('#picture').src = parameter
-    document.querySelector('#picture').class = d-block
 }
+
+function vegosszeg() {
+    a = parseInt (document.querySelector('#tragto').value);
+    b = parseInt (document.querySelector('#extra').value);
+
+   document.querySelector('#osszeg').innerText =insertDecimal(a + b - 1 ) + " Pengő";
+}
+
+function insertDecimal(num) {
+   return Number((num / 1000).toFixed(3));
+}
+
+function insertDecimal2(num) {
+   return Number((num / 1000).toFixed(3));
+}
+//vegosszeg
+
+function Video() {
+  var x = document.getElementById("vid");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+//  video
+
